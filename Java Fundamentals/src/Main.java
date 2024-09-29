a9310cbd8b2b63bbd0f38e078c68fa4f3cf1b614
paulrichiegms <paulrichiegms@gmail.com>
2024-09-29 12:36:47 +0600

    refactor

--- Java Fundamentals/src/Main.java
+++ Java Fundamentals/src/Main.java
@@ -4,100 +4,88 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    final static int month = 12;
+    final static int percentageMultiplier = 100;
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
 
         //Mortgage Calculator::
-        final int month=12;
-        final int percentageMultiplier=100;
+
         int principal;
         double interestRate;
         int period;
         System.out.println("Hello and welcome!");
-
         Scanner sc = new Scanner(System.in);
+        //MortgageCalculator:
+        principal = (int) readNumber("Principal Amount", 1000000);
+        interestRate = readNumber("Interest Rate", 10);
+        period = (int) readNumber("Period", 30);
+        System.out.println("Mortgage Amount: " + mortgageCalculator(principal, interestRate, period));
 
-        while(true){
-            System.out.print("Principal Amount: ");
-            principal= sc.nextInt();
-            if(principal <= 0){
-                System.out.println("Principal Amount cannot be negative");
+        //FizzBuzz:
+        System.out.print("Enter Number for fizzbuzz check: ");
+        int fizzBuzzNumber = sc.nextInt();
+        System.out.println(fizzBuzz(fizzBuzzNumber));
+
+        //QuitChecker:
+        quitChecker();
+
+    }
+
+    public static String mortgageCalculator(int principal, double interestRate, int period) {
+        int totalMonths = period * month;
+        double monthlyInterest = (interestRate / percentageMultiplier) / month;
+
+        double mortgage = principal *
+                (monthlyInterest * Math.pow((1 + monthlyInterest), totalMonths)) /
+                (Math.pow((1 + monthlyInterest), totalMonths) - 1);
+        return NumberFormat.getCurrencyInstance().format(mortgage);
+    }
+
+    public static double readNumber(String prompt, int max) {
+        Scanner sc = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt + ": ");
+            value = sc.nextDouble();
+            if (value <= 0) {
+                System.out.println(prompt + " cannot be negative ");
                 continue;
-            }
-            else if(principal > 100000){
-                System.out.println("Principal cannot be greater than 100000");
+            } else if (value > max) {
+                System.out.println(prompt + " cannot be greater than " + max);
                 continue;
-            }
-            else break;
+            } else break;
         }
+        return value;
+    }
 
-        while(true){
-            System.out.print("Interest Rate: ");
-            interestRate= sc.nextDouble();
-            if(interestRate <= 0){
-                System.out.println("Interest Rate cannot be negative");
-                continue;
+    public static String fizzBuzz(int number) {
+        String value;
+        if (number % 5 == 0) {
+            if (number % 3 == 0) {
+                value="FizzBuzz";
+
+            } else {
+                value="Fizz";
             }
-            else if(interestRate > 20){
-                System.out.println("Interest Rate cannot be greater than 20");
-                continue;
-            }
-            else break;
+        } else if (number % 3 == 0) {
+            value="Buzz";
+        } else {
+            value=Integer.toString(number);
         }
+        return value;
+    }
 
-        while(true){
-            System.out.print("Period: ");
-            period= sc.nextInt();
-            if(period <= 0){
-                System.out.println("Period cannot be negative");
-                continue;
-            }
-            else if(period > 20){
-                System.out.println("Period cannot be greater than 30");
-                continue;
-            }
-            else break;
+    public static void quitChecker() {
+        String input = "";
+        Scanner sc = new Scanner(System.in);
+        while(!input.equals("quit")) {
+            System.out.print("Enter Value for quitChecker: ");
+            input=sc.nextLine().toLowerCase();
+            System.out.println(input);
         }
-
-
-        int totalMonths = period*month;
-        double monthlyInterest =(interestRate/percentageMultiplier)/month;
-
-        double mortgage= principal *
-                (monthlyInterest *Math.pow((1+ monthlyInterest),totalMonths))/
-                (Math.pow((1+ monthlyInterest),totalMonths)-1);
-
-        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Total Mortgage :"+ formattedMortgage);
-
-        //Fizbuzz::
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter Number: ");
-//        int number = sc.nextInt();
-//        if (number % 5 == 0) {
-//            if (number % 3 == 0) {
-//                System.out.println("FizzBuzz");
-//            }
-//            else{
-//                System.out.println("Fizz");
-//            }
-//        }
-//        else if (number % 3 == 0) {
-//            System.out.println("Buzz");
-//        }
-//        else{
-//            System.out.println(number);
-//        }
-
-        //While loop string equal check
-//        Scanner sc = new Scanner(System.in);
-//        String input = "";
-//        while(!input.equals("quit")) {
-//            System.out.print("Enter Value: ");
-//            input = sc.nextLine().toLowerCase();
-//            System.out.println(input);
-//        }
     }
 }
